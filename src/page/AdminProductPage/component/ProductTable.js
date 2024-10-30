50a4f11dcc79b1fb37e623c78d759af77e73a227
EunseoKim <dmstj9408@gmail.com>
2024-10-30 21:15:10 +0900

    [style]search result가 없을 때 문구 수정 및 문구에 searchKeyword 추가

--- src/page/AdminProductPage/component/ProductTable.js
+++ src/page/AdminProductPage/component/ProductTable.js
@@ -3,57 +3,61 @@ import Button from "react-bootstrap/Button";
 import Table from "react-bootstrap/Table";
 import { currencyFormat } from "../../../utils/number";
 
-const ProductTable = ({ header, data, deleteItem, openEditForm }) => {
+const ProductTable = ({ header = [], data = [], deleteItem, openEditForm, searchQuery }) => {
   return (
     <div className="overflow-x">
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            {header.map((title, index) => (
-              <th key={index}>{title}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {data.length > 0 ? (
-            data.map((item, index) => (
-              <tr key={index}>
-                <th>{index}</th>
-                <th>{item.sku}</th>
-                <th style={{ minWidth: "100px" }}>{item.name}</th>
-                <th>{currencyFormat(item.price)}</th>
-                <th>
-                  {Object.keys(item.stock).map((size, index) => (
-                    <div key={index}>
-                      {size}:{item.stock[size]}
-                    </div>
-                  ))}
-                </th>
-                <th>
-                  <img src={item.image} width={100} alt="image" />
-                </th>
-                <th>{item.status}</th>
-                <th style={{ minWidth: "100px" }}>
-                  <Button
-                    size="sm"
-                    variant="danger"
-                    onClick={() => deleteItem(item._id)}
-                    className="mr-1"
-                  >
-                    -
-                  </Button>
-                  <Button size="sm" onClick={() => openEditForm(item)}>
-                    Edit
-                  </Button>
-                </th>
+      {data.length > 0 ? (
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                {header.map((title, index) => (
+                  <th key={index}>{title}</th>
+                ))}
               </tr>
-            ))
-          ) : (
-            <tr>No Data to show</tr>
-          )}
-        </tbody>
-      </Table>
+            </thead>
+            <tbody>
+              {data.map((item, index) => (
+                <tr key={index}>
+                  <th>{index + 1}</th>
+                  <th>{item.sku}</th>
+                  <th style={{ minWidth: "100px" }}>{item.name}</th>
+                  <th>{currencyFormat(item.price)}</th>
+                  <th>
+                    {Object.keys(item.stock).map((size, index) => (
+                      <div key={index}>
+                        {size}: {item.stock[size]}
+                      </div>
+                    ))}
+                  </th>
+                  <th>
+                    <img src={item.image} width={100} alt="product" />
+                  </th>
+                  <th>{item.status}</th>
+                  <th style={{ minWidth: "100px" }}>
+                    <Button
+                      size="sm"
+                      variant="danger"
+                      onClick={() => deleteItem(item._id)}
+                      className="mr-1"
+                    >
+                      -
+                    </Button>
+                    <Button size="sm" onClick={() => openEditForm(item)}>
+                      Edit
+                    </Button>
+                  </th>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+      ) : (
+        <div>
+          <h1>"{searchQuery}"에 대한 결과를 찾을 수 없습니다.</h1>
+          <p>다른 단어를 사용하여 다시 시도하세요.</p>
+        </div>
+      )}
     </div>
   );
 };
+
 export default ProductTable;
